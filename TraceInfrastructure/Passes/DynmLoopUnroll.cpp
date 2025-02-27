6c3f83305e1c9e98f6c5b1d4fceae6681cfff168
chang55245 <lchang21@asu.edu>
2025-02-26 23:29:31 -0700

    new loop unrolling

--- TraceInfrastructure/Passes/DynmLoopUnroll.cpp
+++ TraceInfrastructure/Passes/DynmLoopUnroll.cpp
@@ -22,7 +22,7 @@
 #include <llvm/Pass.h>
 #include <llvm/Transforms/Utils/BasicBlockUtils.h>
 #include <llvm/Transforms/Utils/UnrollLoop.h>
-#include <system_error>
+
 
 
 
@@ -38,167 +38,207 @@ using namespace std;
 
 namespace DashTracer::Passes
 {
-    map<uint64_t, uint64_t> loopIteration;
-    bool DynmLoopUnroll::runOnFunction(Function &F)
-    {
 
-        DominatorTree DT(F);
-        LoopInfo LI(DT);
-        AssumptionCache AC(F);
-        TargetLibraryInfoImpl TLII;
-        TargetLibraryInfo TLI(TLII);
-        ScalarEvolution SE(F, TLI, AC, DT, LI);
-        OptimizationRemarkEmitter ORE(&F);
-        UnrollLoopOptions ULO;
 
-        ULO.Count = 5;
-        ULO.TripCount = ULO.Count;
-        ULO.Force = false;
-        ULO.AllowRuntime = false;
-        ULO.AllowExpensiveTripCount = false;
-        ULO.PreserveCondBr = false;
-        ULO.PreserveOnlyFirst = false;
-        ULO.TripMultiple = 0;
-        ULO.PeelCount = ULO.Count;
-        ULO.UnrollRemainder = true;
-        ULO.ForgetAllSCEV = true;
-
-        // LI.Blocks
-
-        if (LI.empty())
-        {
-            return false;
+        void DynmLoopUnrollPass::initializeUnrollOptions() {
+            ULO.Count = 5;
+            ULO.TripCount = ULO.Count;
+            ULO.Force = false;
+            ULO.AllowRuntime = false;
+            ULO.AllowExpensiveTripCount = false;
+            ULO.PreserveCondBr = false;
+            ULO.PreserveOnlyFirst = false;
+            ULO.TripMultiple = 0;
+            ULO.PeelCount = ULO.Count;
+            ULO.UnrollRemainder = true;
+            ULO.ForgetAllSCEV = true;
         }
 
-        
-        while (true)
-        {
-            bool unrolled = false;
-            for (auto &bb : F)
-            {
-                for (BasicBlock::iterator BI = bb.begin(), BE = bb.end(); BI != BE; ++BI)
-                {
-                    if (auto *CI = dyn_cast<CallInst>(BI))
-                    {
-                        Function *calledFunc = CI->getCalledFunction();
-                        if (calledFunc->getName() == "LoopTrace")
-                        {
-                            auto *loopTest = LI.getLoopFor(&bb);
-                            if (loopTest != nullptr)
-                            {
+        void DynmLoopUnrollPass::initializeAnalysis(Function &F) {
+            DT = new DominatorTree(F);
+            LI = new LoopInfo(*DT);
+            AC = new AssumptionCache(F);
+            TargetLibraryInfoImpl* TLII = new TargetLibraryInfoImpl();
+            TLI = new TargetLibraryInfo(*TLII);
+            SE = new ScalarEvolution(F, *TLI, *AC, *DT, *LI);
+            ORE = new OptimizationRemarkEmitter(&F);
+        }
+
+        void DynmLoopUnrollPass::cleanupAnalysis() {
+            delete DT;
+            delete LI;
+            delete AC;
+            delete TLI;
+            delete SE;
+            delete ORE;
+        }
+
+        // Find all loops and their LoopTrace calls
+        map<Loop*, uint64_t> DynmLoopUnrollPass::findLoopsWithTraces(Function &F) {
+            map<Loop*, uint64_t> loopMap;
+            for (auto &BB : F) {
+                if (Loop* L = LI->getLoopFor(&BB)) {
+                    for (auto &I : BB) {
+                        if (auto *CI = dyn_cast<CallInst>(&I)) {
+                            if (CI->getCalledFunction() && 
+                                CI->getCalledFunction()->getName() == "LoopTrace") {
                                 auto arg = CI->arg_begin();
-                                const Use *use = arg;
-                                Value *val = use->get();
-                                ConstantInt *cons = dyn_cast<ConstantInt>(val);
-
-                                if (loopIteration[cons->getSExtValue()]==0) {
-                                    goto endloop;
+                                if (auto* cons = dyn_cast<ConstantInt>(arg->get())) {
+                                    uint64_t loopId = cons->getSExtValue();
+                                    loopMap[L] = loopId;
                                 }
-
-                                ULO.Count = loopIteration[cons->getSExtValue()];
-                                ULO.TripCount = ULO.Count+1;
-                                // delete loopTrace
-                                CI->eraseFromParent();
-
-                                bool PreserveLCSSA = loopTest->isRecursivelyLCSSAForm(DT, LI);
-                                // errs()<<*loopTest;
-
-
-                                Value *IndVar;
-                                
-                               
-                                BasicBlock *Header = loopTest->getHeader();
-                                Instruction *Term = Header->getTerminator();
-                                
-                                for (BasicBlock::iterator BIi = Header->begin(), BEi = Header->end(); BIi != BEi; ++BIi)
-                                {
-                                    if (auto *CIi = dyn_cast<CmpInst>(BIi)) {
-                                        auto cmpVar = CIi->getOperand(0);
-                                        errs()<<"Found cmpVar: "<<*cmpVar<<"\n";
-                                        errs()<<"Found CI: "<<*CIi<<"\n";
-                                        errs()<<"Found terminaler: "<<*(Term)<<"\n";
-                                        if (CIi == Term->getOperand(0)) {
-                                            
-
-                                            IndVar = cmpVar;
-                                            
-                                            Value *NewTripCount = ConstantInt::get(IndVar->getType(), ULO.Count);
-
-                                            IRBuilder<> Builder(Header);
-
-                                            Builder.SetInsertPoint(CIi);
-                                            
-                                            Value *NewCond = Builder.CreateICmpSLT(IndVar, NewTripCount);
-
-                                            
-                                            
-                                            BranchInst *NewTerm = BranchInst::Create(Term->getSuccessor(0),
-                                                                                Term->getSuccessor(1), NewCond);
-                                            ReplaceInstWithInst(Term, NewTerm);
-                                                         
-                                            CIi->eraseFromParent();
-                                            // errs()<<"Found NewCond: "<<*NewCond<<"\n";
-                                            // errs()<<"Found NewTerm: "<<*NewTerm<<"\n";             
-                                            break;
-                                        }
-                                    }
-                                }           
-                                
-
-                                LI.verify(DT);                                
-                                UnrollLoop(loopTest, ULO, &LI, &SE, &DT, &AC, &ORE, PreserveLCSSA, nullptr);
-                                simplifyLoopAfterUnroll(loopTest, true,&LI, &SE, &DT, &AC);
-                                unrolled = true;
-                                goto endloop;
-                                // clean the binary, delete the everything irrelavate
                             }
-                        }
-                        else if (calledFunc->getName() == "LoopTraceInitialization")
-                        {
-                            CI->eraseFromParent();
-                            unrolled = true;
-                            goto endloop;
-                        }
-                        else if (calledFunc->getName() == "LoopTraceDestroy")
-                        {
-                            CI->eraseFromParent();
-                            unrolled = true;
-                            goto endloop;
                         }
                     }
                 }
             }
-
-
-        endloop:
-            if (!unrolled) {
-            break;
-            }
+            return loopMap;
         }
 
-        // errs() << F;
-        return true;
-    }
+        // Modify loop bound for unrolling
+        bool DynmLoopUnrollPass::modifyLoopBound(Loop* L, uint64_t count) {
+            BasicBlock* Header = L->getHeader();
+            for (auto& I : *Header) {
+                if (auto *CI = dyn_cast<CmpInst>(&I)) {
+                    if (CI == Header->getTerminator()->getOperand(0)) {
+                        Value* IndVar = CI->getOperand(0);
+                        Value* NewTripCount = ConstantInt::get(IndVar->getType(), count);
+                        
+                        IRBuilder<> Builder(Header);
+                        Builder.SetInsertPoint(CI);
+                        Value* NewCond = Builder.CreateICmpSLT(IndVar, NewTripCount);
+                        
+                        BranchInst* Term = cast<BranchInst>(Header->getTerminator());
+                        BranchInst* NewTerm = BranchInst::Create(
+                            Term->getSuccessor(0),
+                            Term->getSuccessor(1),
+                            NewCond
+                        );
+                        ReplaceInstWithInst(Term, NewTerm);
+                        CI->eraseFromParent();
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
 
-    bool DynmLoopUnroll::doInitialization(Module & /*unused*/)
-    {
-        nlohmann::json j;
-        std::ifstream inputStream(LoopTraceFile);
-        inputStream >> j;
-        inputStream.close();
-        nlohmann::json mapping;
-        mapping = j["loopIteration"];
-        loopIteration = mapping.get<map<uint64_t, uint64_t>>();
-        return false;
-    }
+        // Process a single loop
+        bool DynmLoopUnrollPass::processLoop(Loop* L, uint64_t loopId) {
+            if (loopIteration.find(loopId) == loopIteration.end()) {
+                return false;
+            }
 
-    void DynmLoopUnroll::getAnalysisUsage(AnalysisUsage &AU) const
-    {
-        AU.addRequired<DashTracer::Passes::EncodedAnnotate>();
-        AU.setPreservesCFG();
-    }
-    char DynmLoopUnroll::ID = 1;
-    static RegisterPass<DynmLoopUnroll> Z("DynmLoopUnroll",
-                                          "DynmLoopUnroll",
-                                          true, false);
+            ULO.Count = loopIteration[loopId].count;
+            ULO.TripCount = ULO.Count + 1;
+
+            if (!modifyLoopBound(L, ULO.Count)) {
+                return false;
+            }
+
+            bool PreserveLCSSA = L->isRecursivelyLCSSAForm(*DT, *LI);
+            UnrollLoop(L, ULO, LI, SE, DT, AC, ORE, PreserveLCSSA, nullptr);
+            simplifyLoopAfterUnroll(L, true, LI, SE, DT, AC);
+
+            // Remove LoopTrace calls for this loop
+            for (auto& BB : L->getBlocks()) {
+                for (auto I = BB->begin(); I != BB->end();) {
+                    if (auto* CI = dyn_cast<CallInst>(&*I)) {
+                        if (CI->getCalledFunction() &&
+                            CI->getCalledFunction()->getName() == "LoopTrace") {
+                            I = CI->eraseFromParent();
+                            continue;
+                        }
+                    }
+                    ++I;
+                }
+            }
+
+            return true;
+        }
+
+        // Recursively process nested loops
+        bool DynmLoopUnrollPass::processNestedLoops(Loop* L, uint64_t loopId, map<Loop*, uint64_t>& loopMap) {
+            bool modified = false;
+
+            // Process child loops first
+            for (Loop* SubL : L->getSubLoops()) {
+                if (loopMap.count(SubL)) {
+                    uint64_t subLoopId = loopMap[SubL];
+                    if (loopIteration[subLoopId].parentId == loopId) {
+                        modified |= processNestedLoops(SubL, subLoopId, loopMap);
+                    }
+                }
+            }
+
+            // Then process this loop
+            modified |= processLoop(L, loopId);
+            return modified;
+        }
+
+
+        bool DynmLoopUnrollPass::doInitialization(Module& M) {
+            nlohmann::json j;
+            std::ifstream inputStream(LoopTraceFile);
+            inputStream >> j;
+            inputStream.close();
+
+            for (const auto& loop : j["loopIteration"]) {
+                uint64_t loopId = loop["loopId"];
+                loopIteration[loopId] = {
+                    loop["count"],
+                    loop["parentId"]
+                };
+            }
+            return false;
+        }
+
+        bool DynmLoopUnrollPass::runOnFunction(Function &F) {
+            if (F.empty()) return false;
+
+            initializeUnrollOptions();
+            initializeAnalysis(F);
+
+            bool modified = false;
+            auto loopMap = findLoopsWithTraces(F);
+
+            // Start with top-level loops
+            for (Loop* L : *LI) {
+                if (loopMap.count(L)) {
+                    uint64_t loopId = loopMap[L];
+                    if (loopIteration[loopId].parentId == 0) {
+                        modified |= processNestedLoops(L, loopId, loopMap);
+                    }
+                }
+            }
+
+            // Clean up any remaining LoopTrace-related calls
+            for (auto& BB : F) {
+                for (auto I = BB.begin(); I != BB.end();) {
+                    if (auto* CI = dyn_cast<CallInst>(&*I)) {
+                        if (CI->getCalledFunction()) {
+                            StringRef Name = CI->getCalledFunction()->getName();
+                            if (Name == "LoopTraceInitialization" || 
+                                Name == "LoopTraceDestroy") {
+                                I = CI->eraseFromParent();
+                                modified = true;
+                                continue;
+                            }
+                        }
+                    }
+                    ++I;
+                }
+            }
+
+            cleanupAnalysis();
+            return modified;
+        }
+
+        void DynmLoopUnrollPass::getAnalysisUsage(AnalysisUsage &AU) const {
+            AU.addRequired<DashTracer::Passes::EncodedAnnotate>();
+            AU.setPreservesCFG();
+        }
+
+    char DynmLoopUnrollPass::ID = 1;
+    static RegisterPass<DynmLoopUnrollPass> Z("DynmLoopUnroll", "DynmLoopUnroll", true, false);
 } // namespace DashTracer::Passes
